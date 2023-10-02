7710e5bd5a9bb508610d56ef7e194003d444adbf
12thgenpenguin <cjgibb@protonmail.com>
2023-10-03 00:42:22 +1000

    v1.1 - add functions,error checking, optimize code

--- pywal-trigger.sh
+++ pywal-trigger.sh
@@ -1,27 +1,57 @@
 #!/bin/bash
 
-# Define an array of possible backend names
-backends=("haishoku" "colorz" "colorthief" "wal")
+# Check dependencies are in $PATH
+commands=("swaybg" "wal")
 
-selected_backend=${backends[$(shuf -i 0-3 -n 1)]}
-process_id=$(pgrep -o swaybg)
-
-if [ -n "$process_id" ]; then
-    command_line=$(ps -o cmd= -p "$process_id")
-
-    if echo "$command_line" | grep -q "swaybg"; then
-        path=$(echo "$command_line" | awk '{for(i=5;i<=NF;++i) printf "%s ", $i}' | sed 's/ $//')
-
-        if [ -n "$path" ]; then
-            wal -i "${path[@]}" --backend "$selected_backend"
-            notify-send -i /usr/share/icons/pywal.png "Backend: $selected_backend"
-            notify-send -i /usr/share/icons/pywal.png "Wallpaper: $(basename "${path[@]}")"
-        else
-            notify-send "No path found in the command line of swaybg."
-        fi
-    else
-        notify-send "Error: Unable to find 'swaybg' in the command line of swaybg."
+for cmd in "${commands[@]}"; do
+    if ! command -v "$cmd" &> /dev/null; then
+        echo "Error: $cmd not found in PATH. Exiting."
+        exit 1
     fi
+done
+
+# Get the image path being used by swaybg
+get_swaybg_path() {
+local command_line=$(pgrep -o -a swaybg | awk '{print substr($0, index($0,$3))}')
+
+    if [ -z "$command_line" ]; then
+        notify-send "Error: swaybg process not found."
+        exit 1
+    fi
+
+
+    local path=$(echo "$command_line" | awk '{for(i=4;i<=NF;++i) printf "%s ", $i}' | sed 's/ $//')
+
+    if [ -z "$path" ]; then
+        notify-send "Error: Unable to fetch image path from swaybg."
+        exit 1
+    fi
+
+    echo "$path"
+}
+
+# Setting the wallpaper
+set_wallpaper() {
+    local path="$1"
+    local backend="$2"
+
+    wal -i "$path" --backend "$backend"
+    if command -v wal-telegram &> /dev/null; then
+        wal-telegram --wal --tiled
+    fi
+
+    local message=$(echo -e "Wallpaper: $(basename "$path")\nBackend: $backend")
+    notify-send -i /usr/share/icons/pywal.png "$message"
+}
+
+
+# Define backends
+backends=("haishoku" "colorz" "colorthief" "wal")
+selected_backend=${backends[$(shuf -i 0-3 -n 1)]}
+
+path=$(get_swaybg_path)
+if [ -n "$path" ]; then
+    set_wallpaper "$path" "$selected_backend"
 else
-    notify-send "Error: swaybg process not found."
+    notify-send "No path found from the command line of swaybg."
 fi
--- pywal-wallpaper-trigger.sh
+++ pywal-wallpaper-trigger.sh
@@ -1,41 +1,67 @@
 #!/bin/bash
 
+# Check if deps are in $PATH
+commands=("swaybg" "wal")
+
+for cmd in "${commands[@]}"; do
+    if ! command -v "$cmd" &> /dev/null; then
+        echo "Error: $cmd not found in PATH. Exiting."
+        exit 1
+    fi
+done
+
 # Directory to search for image files
 directory="~/wallpapers"
 
-# Find all image files in the specified directory and its subdirectories
-readarray -d '' image_files < <(find "$directory" -type f \( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" \) -print0 2>/dev/null)
+# Function to get a random image from the specified directory
+get_random_image() {
+    # Find a random image file in the specified directory and its subdirectories
+    local random_image=$(find "$directory" -type f \( -iname "*.jpg" -o -iname "*.jpeg" -o -iname "*.png" \) 2>/dev/null | sort -R | head -n 1)
 
-# Check if any image files were found
-if [ ${#image_files[@]} -eq 0 ]; then
-    notify-send "No image files found in $directory or its subdirectories."
-    exit 1
-fi
+    # Check if an image file was found
+    if [ -z "$random_image" ]; then
+        notify-send "No image files found in $directory or its subdirectories."
+        exit 1
+    fi
 
-# Kill running swaybg instances
-pkill swaybg
+    echo "$random_image"
+}
 
-# Get a random image file
-random_image=$(shuf -n 1 -e "${image_files[@]}")
+# Function to set the wallpaper
+set_wallpaper() {
+    local image_path="$1"
 
-# Get the absolute path of the random image
+    # Kill running swaybg instances
+    pkill swaybg
+
+    # Check if swaybg was successfully killed
+    if pgrep -x "swaybg" > /dev/null; then
+        notify-send "Error: Failed to kill existing swaybg process."
+        exit 1
+    fi
+
+    # Set the wallpaper using swaybg
+    swaybg -m fill -i "$image_path" &
+}
+
+random_image=$(get_random_image)
 absolute_path=$(realpath "$random_image")
 
-# Set the wallpaper using swaybg
-swaybg -m fill -i "$absolute_path" &
+set_wallpaper "$absolute_path"
 
 # Define an array of possible backend names
 backends=("haishoku" "colorz" "colorthief" "wal")
-
-# Select a random backend from the array
 selected_backend=${backends[$(shuf -i 0-3 -n 1)]}
 
 # Run the "wal" command with the extracted path and random backend
 wal -i "$absolute_path" --backend "$selected_backend"
 
-# Send a notification with the selected backend name
-notify-send -i /usr/share/icons/pywal.png "Backend: $selected_backend"
+# Only run wal-telegram if it is present in $PATH
+if command -v wal-telegram &> /dev/null; then
+    wal-telegram --wal --tiled
+fi
 
-# Send a notification with the file name
+# Send a notification
 file_name=$(basename "$absolute_path")
-notify-send -i /usr/share/icons/pywal.png "Wallpaper: $file_name"
+message=$(echo -e "Wallpaper: $file_name\nBackend: $selected_backend")
+notify-send -i /usr/share/icons/pywal.png "$message"
